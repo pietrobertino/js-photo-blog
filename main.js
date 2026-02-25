f3bf4e80320d9a01779e79799ab4f4ca33b6c18f
pietrobertino <pietrobertino20@gmail.com>
2026-02-25 12:41:05 +0100

    complete milestone 3

---
+++ main.js
@@ -0,0 +1,22 @@
+const cardImgNodes = document.querySelectorAll(".card-img");
+const cardTitleNodes = document.querySelectorAll(".card-title");
+const cardDateNodes = document.querySelectorAll(".card-text");
+
+console.log(cardImgNodes, cardTitleNodes, cardDateNodes);
+
+
+fetch("https://lanciweb.github.io/demo/api/pictures/")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        for (let i=0; i<data.length; i++) {
+            const currentData = data[i];
+            cardImgNodes[i].src = currentData.url;
+            cardTitleNodes[i].innerText = currentData.title;
+            cardDateNodes[i].innerText = currentData.date;
+        }
+
+    })
+    .catch(error => {
+        console.error(error);
+    });
